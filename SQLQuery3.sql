57f00eaed081798584bb37cff690145182a40278
Karthikeyanelangovan <88783056+Karthikeyanelangovan@users.noreply.github.com>
2021-08-11 19:27:54 +0530

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,9 @@
+select * from IvyCompany
+
+
+Insert into IvyCompany values (2 ,'Raja',3,'Chennai','Average')
+
+delete  from IvyCompany  where [Employee ID] = 2
+
+update IvyCompany set [Emp Rating] = 5 where  [Employee ID] = 45
+
